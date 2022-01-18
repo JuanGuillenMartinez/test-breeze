06f12f16b32e15a5d17fe89376de860dc0c686d4
JuanGuillenMartinez <juanguillenmtz16@gmail.com>
2022-01-18 14:04:39 -0600

    Cambios en la migracion de la tabla status

---
+++ database/migrations/2022_01_18_195607_update_status_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('status', function (Blueprint $table) {
+            //
+            $table->rename('statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('status', function (Blueprint $table) {
+            //
+        });
+    }
+}
