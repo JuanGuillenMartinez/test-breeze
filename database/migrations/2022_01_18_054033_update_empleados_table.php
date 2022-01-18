b218cefc41c6ce56acbf38c9e2adf3437c279dda
JuanGuillenMartinez <juanguillenmtz16@gmail.com>
2022-01-17 23:57:15 -0600

    Creada migracion añadiendo llave foranea de sucursal en la tabla de empleados

---
+++ database/migrations/2022_01_18_054033_update_empleados_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('empleados', function (Blueprint $table) {
+            //
+            $table->unsignedBigInteger('sucursal_id')->after('user_id');
+            $table->foreign('sucursal_id')->references('id')->on('sucursales');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('empleados', function (Blueprint $table) {
+            //
+        });
+    }
+}
