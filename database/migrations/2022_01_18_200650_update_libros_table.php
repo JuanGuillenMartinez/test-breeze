ff1c45f037e84526704209079162e768ca3eb0b1
JuanGuillenMartinez <juanguillenmtz16@gmail.com>
2022-01-18 14:10:22 -0600

    Creado cambios en la migracion de libros

---
+++ database/migrations/2022_01_18_200650_update_libros_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateLibrosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('libros', function (Blueprint $table) {
+            //
+            $table->foreignId('status_id')->after('id')->constrained();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('libros', function (Blueprint $table) {
+            //
+        });
+    }
+}
